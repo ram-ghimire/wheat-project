5815983307e93ed38b8dea8a5271af0a1d7d9afc
ram-ghimire <ramghimire71@gmail.com>
2020-04-18 21:33:05 +1000

    plots added

---
+++ ct_SCRIPTS.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+view(mtcars)
+
+heatmap(as.matrix(mtcars)
+,scale="column"
+,col=heat.colors(256)
+,main="Characteristics of Car Models"
+,Rowv = NA
+,Colv 
+
+library(tidyverse)
+BUNYIP_15 <- read.csv("BUNYIP_CT_15.csv")
+BUNYIP_15
+
+heatmap(as.matrix(BUNYIP_15)
+        ,scale = "column"
+        ,main = "Average Canpy Temperature of BUNYIP 2015"
+        ,Rowv = NA
+        ,Colv = NA
+        )
+
